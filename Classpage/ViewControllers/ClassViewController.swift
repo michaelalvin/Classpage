211c66dbb3915b8135a467a88030bff2e88da5f1
michaelalvin <michael.alvin@stonybrook.edu>
2016-07-15 14:57:19 -0400

    ClassViewController

---
+++ Classpage/ViewControllers/ClassViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ClassViewController.swift
+//  Classpage
+//
+//  Created by Michael Alvin on 7/15/16.
+//  Copyright © 2016 Make School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ClassViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+}
+}
